696b7615b489ca5f71b577ffa3aea8ff7e6ddf4d
hgko1207 <khkkhk1207@gmail.com>
2020-01-14 14:17:06 +0900

    styled components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Router from "Components/Router";
-import Header from "Components/Header";
 
 function App() {
   return (
     <div>
-      <Header />
       <Router />
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-function Header() {
-  return (
-    <header>
-      <ul>
-        <li>
-          <a href="/">Movies</a>
-        </li>
-        <li>
-          <a href="/tv">TV</a>
-        </li>
-        <li>
-          <a href="/search">Search</a>
-        </li>
-      </ul>
-    </header>
-  );
-}
+const Header = styled.header``;
 
-export default Header;
+const List = styled.ul`
+  display: flex;
+  &:hover {
+    background-color: blue;
+  }
+`;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
+
+export default () => (
+  <Header>
+    <List>
+      <Item>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item>
+        <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
+);
