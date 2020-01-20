faf1a39e2989b9e6655e5784cfb59f23c09c4a26
hgko1207 <khkkhk1207@gmail.com>
2020-01-20 17:21:50 +0900

    TVPresenter and Loader Components

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -15,6 +15,9 @@ const Title = styled.span`
 
 const Grid = styled.div`
   margin-top: 25px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 125px);
+  grid-gap: 25px;
 `;
 
 // children은 태그 안에 있는 값들이다. container => title => grid => children
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,22 +2,37 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "Components/Section";
+import Loader from "../../Components/Loader";
 
 const Container = styled.div`
   padding: 0px 10px;
 `;
 
 const HomePresenter = ({ nowPalying, upcoming, popular, error, loading }) =>
-  loading ? null : (
+  loading ? (
+    <Loader />
+  ) : (
     <Container>
       {nowPalying && nowPalying.length > 0 && (
-        <Section title="Now Playing">{nowPalying.map(movie => movie.title)}</Section>
+        <Section title="Now Playing">
+          {nowPalying.map(movie => (
+            <span>{movie.title}</span>
+          ))}
+        </Section>
       )}
       {upcoming && upcoming.length > 0 && (
-        <Section title="Upcoming Movies">{upcoming.map(movie => movie.title)}</Section>
+        <Section title="Upcoming Movies">
+          {upcoming.map(movie => (
+            <span>{movie.title}</span>
+          ))}
+        </Section>
       )}
       {popular && popular.length > 0 && (
-        <Section title="Popular Movies">{popular.map(movie => movie.title)}</Section>
+        <Section title="Popular Movies">
+          {popular.map(movie => (
+            <span>{movie.title}</span>
+          ))}
+        </Section>
       )}
     </Container>
   );
