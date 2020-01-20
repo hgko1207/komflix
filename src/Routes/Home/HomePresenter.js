8517b91d2aa197bfafaa946a06b69e7daf86dba1
hgko1207 <khkkhk1207@gmail.com>
2020-01-20 17:41:31 +0900

    SearchPresenter Component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "../../Components/Loader";
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 0px 20px;
 `;
 
 const HomePresenter = ({ nowPalying, upcoming, popular, error, loading }) =>
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 0px 20px;
 `;
 
 const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
@@ -14,13 +14,25 @@ const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
   ) : (
     <Container>
       {topRated && topRated.length > 0 && (
-        <Section title="Top Rated Shows">{topRated.map(show => show.name)}</Section>
+        <Section title="Top Rated Shows">
+          {topRated.map(show => (
+            <span>{show.name}</span>
+          ))}
+        </Section>
       )}
       {popular && popular.length > 0 && (
-        <Section title="Popular Shows">{popular.map(show => show.name)}</Section>
+        <Section title="Popular Shows">
+          {popular.map(show => (
+            <span>{show.name}</span>
+          ))}
+        </Section>
       )}
       {airingToday && airingToday.length > 0 && (
-        <Section title="Airing Today">{airingToday.map(show => show.name)}</Section>
+        <Section title="Airing Today">
+          {airingToday.map(show => (
+            <span>{show.name}</span>
+          ))}
+        </Section>
       )}
     </Container>
   );
